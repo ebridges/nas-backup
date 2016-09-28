57a84d75da7079e522fcf0343ea6c7d63c44d3f2
Edward Bridges <github@eqbridges.com>
2016-09-27 22:59:46 -0400

    replace tabs with spaces

--- backup-nas.sh
+++ backup-nas.sh
@@ -3,14 +3,14 @@
 VERBOSE=
 if [[ ${1} == '-v' ]];
 then
-	VERBOSE='y'
+  VERBOSE='y'
 fi
 
 declare -a PATHS
 ROOT='/c'
 IFS=$'\n'
 PATHS=($(find ${ROOT} \
-	      -mindepth 2 \
+        -mindepth 2 \
         -maxdepth 2 \
         -type d  2>/dev/null | \
             grep -v 'lost+found' | \
@@ -27,15 +27,15 @@ sort))
 
 for p in "${PATHS[@]}"
 do
-	SYNC_PATH=${p#$(dirname "$(dirname "$p")")/}
-	BUCKET='archive'
-	if [[ ${SYNC_PATH} == multimedia* ]] || [[ ${SYNC_PATH} == photos* ]] ;
-	then	
-		BUCKET='multimedia'
-	fi
-	if [[ ${VERBOSE} ]];
-	then
-		echo "Synchronizing ${ROOT}/${SYNC_PATH} to ${BUCKET}"
-	fi
- 	$(sync-folder ${ROOT}/${SYNC_PATH} ${BUCKET}:${SYNC_PATH})
+  SYNC_PATH=${p#$(dirname "$(dirname "$p")")/}
+  BUCKET='archive'
+  if [[ ${SYNC_PATH} == multimedia* ]] || [[ ${SYNC_PATH} == photos* ]] ;
+  then
+    BUCKET='multimedia'
+  fi
+  if [[ ${VERBOSE} ]];
+  then
+    echo "Synchronizing ${ROOT}/${SYNC_PATH} to ${BUCKET}"
+  fi
+  $(sync-folder ${ROOT}/${SYNC_PATH} ${BUCKET}:${SYNC_PATH})
 done
