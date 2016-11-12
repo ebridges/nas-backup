26912de3cb080e1b63f607a72d6d1dbcdd1a4189
Edward Bridges <github@eqbridges.com>
2016-11-12 13:19:42 -0500

    actually add installation script

--- install.sh
+++ install.sh
@@ -1,1 +1,28 @@
-install.sh
+VERSION=${1}
+
+if [ -z "${VERSION}" ];
+then
+    echo "Usage: ${0} <version>"
+    exit 1
+fi
+
+REPO='nas-backup'
+FILE="v${VERSION}.tar.gz"
+DIST_DIR='./dist'
+CURRENT_LINK='current'
+
+DOWNLOAD_URL="https://github.com/ebridges/${REPO}/archive/${FILE}"
+
+cd ${DIST_DIR}
+echo "[INFO] Downloading archive for version [${VERSION}]."
+wget ${DOWNLOAD_URL}
+echo "[INFO] Extracting archive [${FILE}]."
+tar xzf ${FILE}
+echo "[INFO] Disposing downloaded archive."
+rm -f ${FILE}
+PRIOR=`stat -c '%N' ${CURRENT_LINK}`
+echo "[INFO] Removing link to prior version [${PRIOR}]."
+rm ${CURRENT_LINK}
+echo "[INFO] Linking to new version ['${CURRENT_LINK}' -> '${REPO}-${VERSION}']"
+ln -s ${REPO}-${VERSION} ${CURRENT_LINK}
+echo "[INFO] Done"
