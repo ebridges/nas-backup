4b13ebee756f632b4e49dd3a5611d80d3a63f89d
Edward Bridges <github@eqbridges.com>
2016-11-27 07:32:16 -0500

    add param for cfg to restore script

--- restore-backup.sh
+++ restore-backup.sh
@@ -1,6 +1,9 @@
-SRC=$1
-DEST=$2
+CFG=$1
+SRC=$2
+DEST=$3
 
 # E.G.: restore-backup.sh archive:archived-email ./restore
 
-rclone copy ${SRC} ${DEST} --config etc/rclone-config.cnf --exclude-from etc/exclude.cnf
+rclone copy --config ${CFG} \
+      --exclude-from etc/exclude.cnf \
+      ${SRC} ${DEST}
