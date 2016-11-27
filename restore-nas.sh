4c78bfe51a59b8dcd80e81cf34ae3e51f6b7ea26
Edward Bridges <github@eqbridges.com>
2016-11-27 07:53:13 -0500

    add a comprehensive restore script

---
+++ restore-nas.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+if [ -e ${HOME}/.profile_cron ] ;
+then
+  source ${HOME}/.profile_cron
+fi
+
+CONFIG=${1}
+FILE_LIST=${2}
+
+if [ -z "${CONFIG}" ] || [ -z "${FILE_LIST}" ] ;
+then
+  echo "Usage: $0 [path/to/config] [path/to/file-list]"
+  exit 1
+fi
+
+if [ ! -e "${CONFIG}" ];
+then
+  echo "${CONFIG} not found!"
+  exit 1
+fi
+
+if [ ! -e "${FILE_LIST}" ];
+then
+  echo "${FILE_LIST} not found!"
+  exit 1
+fi
+
+while IFS=, read LOCAL_PATH REMOTE_PATH
+do
+  $(sync-folder "${CONFIG}" "${REMOTE_PATH}" "${LOCAL_PATH}")
+done < ${FILE_LIST}
